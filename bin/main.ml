b6866d0ef3f078a1eb474feba5077b8667515948
Yeonho Jang <siabard@gmail.com>
2021-11-22 17:31:03 +0900

    일반 모양 그리기 및 회전 애니메이션 (mutable data 이용)

--- bin/main.ml
+++ bin/main.ml
@@ -1,21 +1,31 @@
 
 
-let window_width = 1280
-
-let window_height = 720
+let (window_width, window_height) = (1280, 720)
 
 let setup () =
   let open Raylib in
   init_window window_width window_height "my game";
   set_target_fps 60
 
+let rotation = ref 0.0
+
+
 let rec loop () =
   let open Raylib in
+
   match window_should_close () with
   | true -> close_window ()
   | false ->
     begin_drawing ();
-    clear_background(Color.create 255  0  255  255);
+    clear_background(Color.white);
+
+    draw_rectangle 100 100 100 300 Color.red;
+    draw_rectangle_v (Vector2.create 300.0  200.0) (Vector2.create 100.0 30.0) Color.blue;
+
+    (*draw_rectangle_rec (Rectangle.create 200.0 200.0 100.0 400.0) Color.green; *)
+
+    rotation := (!rotation) +. 0.5;
+    draw_rectangle_pro (Rectangle.create 200.0 200.0 100.0 100.0) (Vector2.create 0.0 0.0) (!rotation) Color.green;
     end_drawing ();
     loop ()
 
