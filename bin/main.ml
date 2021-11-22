fc1c9416ec0a54fdcad8f440728375da33c952e0
Yeonho Jang <siabard@gmail.com>
2021-11-22 17:07:54 +0900

    기본 모듈 만들기

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,24 @@
-let () = print_endline "Hello, World!"
+
+
+let window_width = 1280
+
+let window_height = 720
+
+let setup () =
+  let open Raylib in
+  init_window window_width window_height "my game";
+  set_target_fps 60
+
+let rec loop () =
+  let open Raylib in
+  match window_should_close () with
+  | true -> close_window ()
+  | false ->
+    begin_drawing ();
+    clear_background(Color.create 255  0  255  255);
+    end_drawing ();
+    loop ()
+
+
+let () = setup() |> loop
+
